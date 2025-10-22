712ab71bca53a2891677d28d253a822c3fb144c5
abdelkarimanouer <abdelkarimanouer03@gmail.com>
2025-10-22 22:52:04 +0100

    add file

--- ft_split.c
+++ ft_split.c
@@ -33,26 +33,6 @@ static size_t	getsize(char const *s, char c)
 	return (size);
 }
 
-static char	*add_str(const char *s, size_t start, size_t end)
-{
-	size_t		i;
-	char		*copy;
-	size_t		len;
-
-	i = 0;
-	len = end - start;
-	copy = malloc(len + 1);
-	if (copy == NULL)
-		return (NULL);
-	while (i < len)
-	{
-		copy[i] = s[start + i];
-		i++;
-	}
-	copy[i] = '\0';
-	return (copy);
-}
-
 static size_t	skip(size_t i, char c, char const *s)
 {
 	while (c != s[i] && s[i])
@@ -80,7 +60,7 @@ char	**ft_split(char const *s, char c)
 		{
 			start = i;
 			i = skip(i, c, s);
-			arr[index++] = add_str(s, start, i);
+			arr[index++] = ft_substr(s, start, i - start);
 		}
 		else
 			i++;
