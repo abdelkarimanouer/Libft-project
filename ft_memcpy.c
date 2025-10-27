445821b685263f52a3f044b1bae43b2b4f66b5d7
Abdelkarim Anouer <aanouer@e2r3p13.1337.ma>
2025-10-27 09:57:27 +0100

    add file

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:18:26 by aanouer           #+#    #+#             */
-/*   Updated: 2025/10/27 09:45:04 by aanouer          ###   ########.fr       */
+/*   Updated: 2025/10/27 09:57:07 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*pdest;
 	unsigned char	*psrc;
 
-	if (!dest || !src)
-		return (dest);
+	if (!dest && !src)
+		return (NULL);
 	i = 0;
 	pdest = (unsigned char *) dest;
 	psrc = (unsigned char *) src;
